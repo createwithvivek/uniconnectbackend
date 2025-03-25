52473a5ebfade0d13f6fc919f2f3b3c9734e1ad0
Vivek <99064174+createwithvivek@users.noreply.github.com>
2025-03-25 15:45:28 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -15,23 +15,19 @@ class CustomUser(AbstractUser):
     cover_photo = models.ImageField(upload_to='cover_photos/', blank=True, null=True)
     REQUIRED_FIELDS = ['email', 'phone', 'role']
 
-    def __str__(self):
-        return self.username
+    
 
 
-class StudentProfile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='student_profile')
+
     university = models.CharField(max_length=100)
     degree = models.CharField(max_length=100)
     graduation_year = models.PositiveIntegerField()
     skills = models.CharField(max_length=300)
 
-    def __str__(self):
-        return self.user.username
+    
 
 
-class MentorProfile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='mentor_profile')
+
     work_experience = models.TextField()
     company = models.CharField(max_length=100)
     industry = models.CharField(max_length=100)
@@ -41,12 +37,11 @@ class MentorProfile(models.Model):
     open_for_mentorship = models.BooleanField(default=True)
     availability = models.CharField(max_length=100, blank=True, null=True)
 
-    def __str__(self):
-        return self.user.username
+    
 
 
-class InvestorProfile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='investor_profile')
+
+    
     investment_firm = models.CharField(max_length=100, blank=True, null=True)
     investment_categories = models.CharField(max_length=200)
     min_investment = models.DecimalField(max_digits=15, decimal_places=2)
