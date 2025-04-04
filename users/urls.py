6c924dd2ce60d7c6ade62b23fedaf64f3ac4311e
Vivek <99064174+Developer01042003@users.noreply.github.com>
2025-04-04 19:43:42 +0530

    helo

--- users/urls.py
+++ users/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('investor/dashboard/', InvestorDashboardView.as_view(), name='investor_dashboard'),
     path('search/', SearchUserView.as_view(), name='search_user'),
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
-     path('update-profile/', UpdateProfileView.as_view(), name='update_profile'),
+    path('update-profile/', UpdateProfileView.as_view(), name='update_profile'),
 ]
