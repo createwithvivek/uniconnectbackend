8d69a9d762dac898a05f69de8727191beae86a9c
Vivek <99064174+Developer01042003@users.noreply.github.com>
2025-04-27 18:57:48 +0530

    profile

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import CustomUser, StudentProfile, MentorProfile, InvestorProfile
+from .models import CustomUser, StudentProfile, MentorProfile, InvestorProfile,Follow
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['username', 'email', 'role','phone', 'full_name', 'is_active', 'is_staff','profile_picture']
+    readonly_fields = ['profile_picture'] 
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(StudentProfile)
 admin.site.register(MentorProfile)
 admin.site.register(InvestorProfile)
+admin.site.register(Follow)
